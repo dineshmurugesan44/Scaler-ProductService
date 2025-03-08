9f1e8de37788951d11e8331b7633e18c2c6e0a35
dineshmurugesan44 <dineshmurugesan44@gmail.com>
2025-03-08 22:02:18 +0530

    adding FakeStoreProductService,and rest template application configuration and finally dto object.

--- src/main/java/com/scaler/productservice/controller/ProductController.java
+++ src/main/java/com/scaler/productservice/controller/ProductController.java
@@ -1,13 +1,29 @@
 package com.scaler.productservice.controller;
 
+import com.scaler.productservice.model.Product;
+import com.scaler.productservice.service.FakeStoreProductService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
 
+    private FakeStoreProductService service;
+
+    public ProductController(FakeStoreProductService inputService) {
+        this.service = inputService;
+    }
+
+
+
 
     @GetMapping("/products/{id}")
-    public void getProductById(@PathVariable("id") Integer id) {
+    public Product getProductById(@PathVariable("id") Integer id) {
+        // validations
+        if(id == null) {
+            throw new IllegalArgumentException("id cannot be null");
+        }
+
+        return service.getProductById(id);
 
     }
     @PostMapping("/products")
---
+++ src/main/java/com/scaler/productservice/dto/FakeStoreResponseDTO.java
@@ -0,0 +1,17 @@
+package com.scaler.productservice.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FakeStoreResponseDTO {
+
+    private Integer id;
+    private String title;
+    private String category;
+    private String description;
+    private String image;
+
+}
